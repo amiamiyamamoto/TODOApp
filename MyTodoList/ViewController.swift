cca9bfb1e37379911a7399a5bfbc442b909c1a44
AMI <a_yamamoto@innovation.co.jp>
2017-11-21 12:40:58 +0900

    2017/11/21_UIの設定を行った

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//UITableViewDataSourceと、UITabBarDelegateの設定
+class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
